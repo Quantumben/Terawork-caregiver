14b8bb59f4be252442cf6a24678f5357373ddf6c
Benedict Goodluck <omosabenedict@gmail.com>
2023-08-25 07:47:04 +0100

    Add files via upload
    FILE that where not pushed

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "Localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "caregiverreview";
+
+// Site URL
+$site_url = "http://localhost/caregiverreview"; // e.g (http://example.com)
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "a7b2be7d-8f8c-4d03-b47b-19626b107a98"; // Your purchase code, don't give it to anyone.
+?>
